b2c2ce7baf7e7f3aea374b1da6a37b8e62956d05
tomlightning <tomer@cedato.com>
2022-09-14 15:03:42 +0300

    init again

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -1,15 +1,10 @@
-import * as React from 'react'
-import { render } from '@testing-library/react'
+import * as React from "react";
+import { render } from "@testing-library/react";
 
-import 'jest-canvas-mock'
+import "jest-canvas-mock";
 
-import { AnalyticsContextProvider } from '../src'
-
-describe('Common render', () => {
-  it('renders without crashing', () => {
-    render(
-    <AnalyticsContextProvider topic="test_topic" ipfsNodeUrl="https://127.0.0.1:4001" >
-        <p>Hello</p>
-    </AnalyticsContextProvider>)
-  })
-})
+describe("Common render", () => {
+  it("renders without crashing", () => {
+    render(<p>Hello</p>);
+  });
+});
